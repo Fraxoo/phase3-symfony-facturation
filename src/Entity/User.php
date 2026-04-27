848e5177fc73693b40f3b1e874b7f7e79f327e68
Fraxoo <fraxo.officiel@gmail.com>
2026-04-27 14:28:41 +0200

    added register

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -12,7 +12,7 @@ use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_EMAIL', fields: ['email'])]
-#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
+#[UniqueEntity(fields: ['email'], message: 'Il existe déjà un compte avec cette adresse email')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
@@ -47,7 +47,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column(length: 255)]
     private ?string $iban = null;
 
-    #[ORM\Column(length: 255)]
+    #[ORM\Column(length: 255 , nullable: true)]
     private ?string $siret = null;
 
     /**
--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -17,27 +17,60 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('agreeTerms', CheckboxType::class, [
-                'mapped' => false,
-                'constraints' => [
-                    new IsTrue(
-                        message: 'You should agree to our terms.',
-                    ),
+            ->add('email' , null, [
+                'label' => "Adresse Email",
+                'attr' => [
+                    'placeholder' => "Ex : john.doe@example.com"
+                ]
+            ])
+            ->add('first_name' , null, [
+                'label' => "Prénom",
+                'attr' => [
+                    'placeholder' => "Prénom"
+                ]
+            ])
+            ->add('last_name' , null, [
+                'label' => "Nom",
+                'attr' => [
+                    'placeholder' => "Nom"
+                ]
+            ])
+            ->add('company_name' , null, [
+                'label' => "Raison sociale",
+                'attr' => [
+                    'placeholder' => "Ex : Airbus"
+                ]
+            ])
+            ->add('iban' , null, [
+                'label' => "IBAN",
+                'attr' => [
+                    'placeholder' => "Ex : FR76 3000 6000 0112 3456 7890 189"
                 ],
+                'constraints' => [
+                    new NotBlank(
+                        message: 'Veuillez entrer un IBAN',
+                    ),
+                    new Length(
+                        min: 27,
+                        max: 34,
+                        minMessage: 'Votre IBAN doit faire au moins {{ limit }} caractères',
+                        maxMessage: 'Votre IBAN ne peut pas faire plus de {{ limit }} caractères',
+                    ),
+                ]
             ])
             ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
+                'label' => "Mot de passe",
                 'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password'],
+                'attr' => ['autocomplete' => 'new-password' , 'placeholder' => "********"],
                 'constraints' => [
                     new NotBlank(
-                        message: 'Please enter a password',
+                        message: 'Veuillez entrer un mot de passe',
                     ),
                     new Length(
                         min: 6,
-                        minMessage: 'Your password should be at least {{ limit }} characters',
+                        minMessage: 'Votre mot de passe doit faire au moins {{ limit }} caractères',
                         // max length allowed by Symfony for security reasons
                         max: 4096,
                     ),
